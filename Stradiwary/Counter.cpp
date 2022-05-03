77d3212df366c414eaa9cd5e72aac63593139340
Ledatu <zavoronok.danila@gmail.com>
2022-05-03 18:38:18 +0300

    Counter class added

---
+++ Stradiwary/Counter.cpp
@@ -0,0 +1,51 @@
+#include "Counter.h"
+
+// constructors
+Counter::Counter() {
+	m_current = 0;
+	m_max = 0;
+}
+Counter::Counter(int max, int timerInterval) : m_timer(timerInterval) {
+	m_current = 0;
+	m_max = max;
+}
+
+// reset current
+void Counter::reset() {
+	m_current = 0;
+}
+
+// getters
+int Counter::current() {
+	return m_current;
+}
+int Counter::max() {
+	return m_max;
+}
+
+// setters
+void Counter::set_current(int current) {
+	if (current > m_max)
+		m_current = m_max;
+	else
+		m_current = current;
+}
+void Counter::set_max(int max) {
+	if (m_current > max)
+		m_current = max;
+	m_max = max;
+}
+
+// overloads
+int& Counter::operator ++ () {
+	return ++m_current;
+}
+int& Counter::operator ++ (int t) {
+	return operator ++ ();
+}
+int& Counter::operator+=(int k) {
+	return m_current += k;
+}
+int& Counter::operator-=(int k) {
+	return m_current -= k;
+}
---
+++ Stradiwary/Counter.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "Timer.h"
+
+class Counter {
+	int m_current;
+	int m_max;
+	Timer m_timer;
+
+public:
+	Counter();
+	Counter(int max, int timerInterval);
+
+	// reset current to max
+	void reset();
+
+	// getters
+	int current();
+	int max();
+
+	// setters
+	void set_current(int current);
+	void set_max(int max);
+
+	// overloads
+	int& operator ++ ();
+	int& operator ++ (int t);
+	int& operator += (int k);
+	int& operator -= (int k);
+
+};
+
