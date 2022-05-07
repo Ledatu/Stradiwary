08cab6f90e6b6c6baca6c5fcf4f4089e8ad6f535
Ledatu <zavoronok.danila@gmail.com>
2022-05-07 21:04:02 +0300

    Item class added for backpack

---
+++ Stradiwary/Item.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Entity.h"
+#include "Counter.h"
+
+class Item : public Entity {
+	Counter m_count;
+
+public:
+	Item();
+	Item(Item* item);
+	Item(int id, olc::vf2d position, olc::vf2d size, int maxCount);
+
+	// reset Item obj
+	void reset() override;
+
+	// getters
+	Counter& count();
+
+};
+
